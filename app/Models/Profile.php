87ac2b3bd0a36e28e1696e9b28b96d5a10bacdc3
rizalKhisyam <162410101048@cs.unej.ac.id>
2021-04-17 22:09:18 +0700

    Menambahkan use SoftDeletes pada model Profile untuk menggunakan fitur SoftDeletes

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Profile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $fillable = ['username', 'email', 'password', 'last_name', 'first_name', 'address', 'city', 'province', 'country'];
+    protected $dates = ['deleted_at'];
 }
